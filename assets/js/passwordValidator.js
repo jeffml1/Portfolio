bce3af9546b91e2b253e1dba07d7416c1cc3f3f0
jeffml1 <jeffml1@student.op.ac.nz>
2017-11-08 17:47:42 +1300

    Commented code for now

--- assets/js/passwordValidator.js
+++ assets/js/passwordValidator.js
@@ -1,5 +1,8 @@
 function CheckPass()
 {
+  window.location.href = "assets/Matthew_Jeffery_CV_2017.pdf"
+  /*
+  Will uncomment this as soon as project is marked so my CV can be viewed by supervisor
   var pass1 = "fxd2vw8"
   var answer = prompt ("Please enter password to view my CV.")
   var cleanPass = DOMPurify.sanitize(answer);
@@ -13,4 +16,5 @@ function CheckPass()
   {
     confirm ("Please contact me for a password if you wish to view my CV.")
   }
+  */
 }
